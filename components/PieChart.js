03219dac0c8be61fa26e0e0a88af0aea0474b660
Ryan Murray <105310431+murray-dev@users.noreply.github.com>
2023-06-20 16:30:24 +0200

    Add PieChart component without props

--- components/PieChart.js
+++ components/PieChart.js
@@ -3,7 +3,30 @@ import dynamic from 'next/dynamic';
 const Plot = dynamic(() => { return import("react-plotly.js") }, { ssr: false })
 
 export default function PieChart() {
+
+    // Example of data you could pass in as prop
+    const title = "Mmmm pie"
+    const width = 400
+    const height = 400
+    const data = {
+        Work: 20,
+        Fitness: 60,
+        Leisure: 20,
+    }
+
+    const getValues = () => Object.values(data);
+    const getLabels = () => Object.keys(data);
+
     return (
-        <>Placeholder</>
-    )
+        <Plot
+            data={[{
+                type: 'pie',
+                values: getValues(),
+                labels: getLabels(),
+            }
+            ]}
+            layout={{ title: title, width: width, height: height }}
+            config={{ responsive: true, displaylogo: false }}
+        />
+    );
 }
