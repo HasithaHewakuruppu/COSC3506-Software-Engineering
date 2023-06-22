e0b6114e3321121c057aaa1b997f76e7ad553609
Abdul Shabbir <abdulqshabbir@gmail.com>
2023-06-22 18:31:21 +0800

    feat: :heavy_plus_sign: add eslint to catch logical errors in our codebase

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,4 +1,4 @@
-import NextAuth, { NextAuthOptions } from 'next-auth'
+import NextAuth from 'next-auth'
 import { PrismaAdapter } from '@auth/prisma-adapter'
 import { PrismaClient } from '@prisma/client'
 import GoogleProvider from 'next-auth/providers/google'
--- pages/tests/index_v2.js
+++ pages/tests/index_v2.js
@@ -110,7 +110,7 @@ export default function HomePage() {
                   Login
                 </button>
                 <p className="small fw-bold mt-2 pt-1 mb-0">
-                  Don't have an account?&nbsp;&nbsp;
+                  Don&apos;t have an account?&nbsp;&nbsp;
                   <a href="#!" className={`link-danger ${styles.noUnderline}`}>
                     Register
                   </a>
