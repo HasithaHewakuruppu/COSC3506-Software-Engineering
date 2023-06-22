0317d943d1f0473bb59fb90f0e4cb7168bca1d67
Hasitha <hasithahewakuruppu@gmail.com>
2023-06-22 06:54:37 +0530

    add direction to dashboard test

--- pages/index.js
+++ pages/index.js
@@ -1,79 +1,101 @@
-import styles from "../styles/Home.module.css"
+import styles from "../styles/Home.module.css";
 import { signIn } from "next-auth/react";
-
-// i stole this off https://mdbootstrap.com/docs/standard/extended/login/  :) 
-// We can remove things we dont want later, such as forgot password which is hard to implement...
+import { useState } from "react";
+import { PulseLoader } from "react-spinners";
 
 export default function HomePage() {
+  const [isLoading, setIsLoading] = useState(false);
 
-  const handleGoogleAuth = () => {
-    signIn("google");
+  const handleGoogleAuth = async () => {
+    setIsLoading(true);
+    await signIn("google", { callbackUrl: "/tests/dashboard" });
+    
   };
 
-  const handleDiscordAuth = () => {
-    signIn("discord");
+  const handleDiscordAuth = async () => {
+    setIsLoading(true);
+    await signIn("discord", { callbackUrl: "/tests/dashboard" });
+    
   };
 
-  const handleGitHubAuth = () => {
-    signIn("github");
+  const handleGitHubAuth = async () => {
+    setIsLoading(true);
+    await signIn("github", { callbackUrl: "/tests/dashboard" });
+   
   };
-  
+
   return (
-  <section class="vh-100">
-    <div className={`${styles.hcustom} container-fluid`}>
-      <div class="row d-flex justify-content-center align-items-center h-100">
-        <div class="col-md-9 col-lg-6 col-xl-5">
-          <h1>Hi Hello, How are you ?</h1>
-          <img class="img-fluid" alt="Sample image" src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"></img>
+    <section class="vh-100">
+      <div className={`${styles.hcustom} container-fluid`}>
+        <div class="row d-flex justify-content-center align-items-center h-100">
+          <div class="col-md-9 col-lg-6 col-xl-5">
+            <h1>Hi Hello, How are you ?</h1>
+            <img
+              class="img-fluid"
+              alt="Sample image"
+              src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
+            ></img>
+          </div>
+          <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
+            <form>
+              <h2
+                className={`${styles.signinStyles} lead fw-bold mb-0 me-3`}
+              >
+                {isLoading ? (
+                  <PulseLoader color="#0d6efd" />
+                ) : (
+                  "Sign in"
+                )}
+              </h2>
+
+              <div className={`${styles.divider} d-flex align-items-center my-4 `}>
+                <p class="text-center fw-bold mx-3 mb-0 ">with</p>
+              </div>
+
+              <button
+                type="button"
+                className={`${styles.socialLogin} ${styles.google} btn btn-primary btn-lg form-control form-control-lg`}
+                onClick={handleGoogleAuth}
+              >
+                <i class="fab fa-google me-2"></i>&nbsp;&nbsp;Google
+              </button>
+
+              <button
+                type="button"
+                className={`${styles.socialLogin} ${styles.github} btn btn-primary btn-lg form-control form-control-lg`}
+                onClick={handleGitHubAuth}
+              >
+                <i class="fab fa-github me-2"></i>&nbsp;&nbsp;GitHub
+              </button>
+
+              <button
+                type="button"
+                className={`${styles.socialLogin} ${styles.discord} btn btn-primary btn-lg form-control form-control-lg`}
+                onClick={handleDiscordAuth}
+              >
+                <i class="fab fa-discord me-2"></i>&nbsp;&nbsp;Discord
+              </button>
+            </form>
+          </div>
         </div>
-        <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
-          <form>
-        
-            <h2 className={`${styles.signinStyles} lead fw-bold mb-0 me-3`}>Sign in</h2>
-        
-            <div className={`${styles.divider} d-flex align-items-center my-4 `}>
-              <p class="text-center fw-bold mx-3 mb-0 ">with</p>
-            </div>
-            
-                <button type="button" className={`${styles.socialLogin} ${styles.google} btn btn-primary btn-lg form-control form-control-lg`} onClick={handleGoogleAuth}>
-                    <i class="fab fa-google me-2"></i>&nbsp;&nbsp;Google
-                </button>
-
-                <button type="button" className={`${styles.socialLogin} ${styles.github} btn btn-primary btn-lg form-control form-control-lg`} onClick={handleGitHubAuth}>
-                    <i class="fab fa-github me-2"></i>&nbsp;&nbsp;GitHub
-                </button>
-
-                <button type="button" className={`${styles.socialLogin} ${styles.discord} btn btn-primary btn-lg form-control form-control-lg`} onClick={handleDiscordAuth}>
-                    <i class="fab fa-discord me-2"></i>&nbsp;&nbsp;Discord
-                </button>
-
-          </form>
+      </div>
+      <div class="d-flex flex-column flex-md-row text-center text-md-start justify-content-between py-4 px-4 px-xl-5 bg-primary">
+        <div class="text-white mb-3 mb-md-0">Copyright © 2023. All rights reserved.</div>
+        <div>
+          <a href="#!" class="text-white me-4">
+            <i class="fab fa-facebook-f"></i>
+          </a>
+          <a href="#!" class="text-white me-4">
+            <i class="fab fa-twitter"></i>
+          </a>
+          <a href="#!" class="text-white me-4">
+            <i class="fab fa-google"></i>
+          </a>
+          <a href="#!" class="text-white">
+            <i class="fab fa-linkedin-in"></i>
+          </a>
         </div>
       </div>
-    </div>
-    <div
-      class="d-flex flex-column flex-md-row text-center text-md-start justify-content-between py-4 px-4 px-xl-5 bg-primary">
-    
-      <div class="text-white mb-3 mb-md-0">
-        Copyright © 2020. All rights reserved.
-      </div>
-    
-      <div>
-        <a href="#!" class="text-white me-4">
-          <i class="fab fa-facebook-f"></i>
-        </a>
-        <a href="#!" class="text-white me-4">
-          <i class="fab fa-twitter"></i>
-        </a>
-        <a href="#!" class="text-white me-4">
-          <i class="fab fa-google"></i>
-        </a>
-        <a href="#!" class="text-white">
-          <i class="fab fa-linkedin-in"></i>
-        </a>
-      </div>
-      
-    </div>
-  </section>
+    </section>
   );
 }
--- pages/tests/dashboard.js
+++ pages/tests/dashboard.js
@@ -1,26 +1,62 @@
 import styles from '../../styles/DashBoard.module.css';
 import Calendar from '../../components/calendar';
+import { signOut, getSession } from 'next-auth/react';
+import { useState } from 'react';
+import { PulseLoader } from 'react-spinners';
 
-export default function Dashboard() {
-    return (
-        <>
-            <nav className={styles.navbar}>
-                <h1>The Navbar goes here</h1>
-            </nav>
-            
-            <div className={styles.container}>
-                <div className={styles.left}>
-                    <h2>List could go here</h2>
-                </div>
-                <div className={styles.right}>
-                    <div className={`${styles.top} ${styles.fullHeight}`}>
-                        <Calendar />  
-                    </div>
-                    <div className={`${styles.bottom} ${styles.fullHeight}`}>
-                        <h2>Pie Chart could go here</h2>
-                    </div>
-                </div>
-            </div>
-        </>
-    );
+export default function Dashboard({ session }) {
+  const [loggingOut, setLoggingOut] = useState(false);
+
+  if (!session) {
+    if (typeof window !== "undefined") {
+      window.location.href = "/";
+    }
+    return null;
+  }
+
+  const handleLogout = async () => {
+    setLoggingOut(true);
+    await signOut();
+    window.location.href = '/';
+  };
+
+  return (
+    <>
+      <nav className={styles.navbar}>
+        <div className={styles.navbarContent}>
+          <h1>The Navbar goes here</h1>
+          <div className={`${styles.logoutButton}`}>
+            {loggingOut ? (
+              <PulseLoader color="#0d6efd"/>
+            ) : (
+              <button className='btn btn-primary' onClick={handleLogout}>Logout</button>
+            )}
+          </div>
+        </div>
+      </nav>
+
+      <div className={styles.container}>
+        <div className={styles.left}>
+          <h2>List could go here</h2>
+        </div>
+        <div className={styles.right}>
+          <div className={`${styles.top} ${styles.fullHeight}`}>
+            <Calendar />
+          </div>
+          <div className={`${styles.bottom} ${styles.fullHeight}`}>
+            <h2>Pie Chart could go here</h2>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export async function getServerSideProps(context) {
+  const session = await getSession(context);
+  return {
+    props: {
+      session: session ? session : null,
+    },
+  };
 }
