fb6c711f3c4971b021e39929d84453602921f4b7
Hasitha <hasithahewakuruppu@gmail.com>
2023-06-23 21:13:11 +0530

    finish list ui

--- components/List.js
+++ components/List.js
@@ -1,19 +1,38 @@
+import React from 'react'
 import ListItem from './ListItem'
 import styles from '../styles/List.module.css'
 
 export default function List({ items }) {
+  const currentDate = new Date().toLocaleDateString('en-GB')
+
   return (
-    <ul>
-      {items.map((item) => (
-        <ListItem
-          key={item.title} // this is needed for editing and deleting, if you need a unique key
-          title={item.title}
-          duration={item.duration}
-          description={item.description}
-          category={item.label.category}
-          label={item.label.name}
-        />
-      ))}
-    </ul>
+    <div>
+      <div className={styles.upperSubContainer}>
+        <div className={styles.headingContainer}>
+          <div className={styles.faviconContainer}>
+            <i className={`fa fa-list ${styles.favIcon}`}></i>
+            <p className={styles.content}>Task Lists:</p>
+          </div>
+          <p className={styles.currentDate}>{currentDate}</p>
+        </div>
+      </div>
+      <div className={styles.listContainer}>
+        {items.map((item) => (
+          <ListItem
+            key={item.title}
+            title={item.title}
+            duration={item.duration}
+            description={item.description}
+            category={item.label.category}
+            label={item.label.name}
+          />
+        ))}
+      </div>
+      <div className={styles.lowerSubContainer}>
+        <button className={`${styles.button} btn btn-primary`}>
+          <p className={styles.content}>Add Task</p>
+        </button>
+      </div>
+    </div>
   )
 }
