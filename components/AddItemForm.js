bf046ccf1956f403a5776bac97420f344303803e
Hasitha <hasithahewakuruppu@gmail.com>
2023-06-25 16:10:23 +0530

    add pick date to task add

--- components/AddItemForm.js
+++ components/AddItemForm.js
@@ -1,81 +1,95 @@
-import React, { useState } from 'react';
-import useSWR from 'swr';
-import styles from "../styles/AddItemForm.module.css";
-import SessionUser from '../components/SessionUser';
+import React, { useState } from 'react'
+import useSWR from 'swr'
+import DatePicker from 'react-datepicker'
+import styles from '../styles/AddItemForm.module.css'
+import 'react-datepicker/dist/react-datepicker.css'
+import SessionUser from '../components/SessionUser'
 
 function AddItemForm() {
-  const [title, setTitle] = useState('');
-  const [duration, setDuration] = useState('');
-  const [description, setDescription] = useState('');
-  const [labelId, setlabelId] = useState('');
-  const [isLoading, setIsLoading] = useState(false);
+  const [title, setTitle] = useState('')
+  const [hours, setHours] = useState('')
+  const [minutes, setMinutes] = useState('')
+  const [description, setDescription] = useState('')
+  const [labelId, setLabelId] = useState('')
+  const [selectedDate, setSelectedDate] = useState(null)
+  const [isLoading, setIsLoading] = useState(false)
 
-  const fetcher = (url) => fetch(url).then((res) => res.json());
-  const { data: labels, error } = useSWR('http://localhost:3000/api/labels', fetcher);
+  const fetcher = (url) => fetch(url).then((res) => res.json())
+  const { data: labels, error } = useSWR(
+    'http://localhost:3000/api/labels',
+    fetcher
+  )
 
   if (error) {
-    console.error('Error fetching labels:', error);
+    console.error('Error fetching labels:', error)
   }
 
-  const date = new Date();
-
   async function handleSubmit(e) {
-    setIsLoading(true);
-    e.preventDefault();
+    setIsLoading(true)
+    e.preventDefault()
 
-    const sessionUser = await SessionUser(); 
-    const userEmail = sessionUser.email;
+    const sessionUser = await SessionUser()
+    const userEmail = sessionUser.email
 
     const newItem = {
       title,
-      duration,
+      duration: hours * 60 + minutes,
       description,
       labelId,
-      date,
-      userEmail
-    };
-
-    try{
-    await fetch('http://localhost:3000/api/todos', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(newItem)
-    });} catch (error) {
-      alert('Error adding item:', error);
+      date: selectedDate,
+      userEmail,
     }
 
-    setIsLoading(false);
+    try {
+      await fetch('http://localhost:3000/api/todos', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newItem),
+      })
+    } catch (error) {
+      alert('Error adding item:', error)
+    }
 
-    setTitle('');
-    setDuration('');
-    setDescription('');
-    setlabelId('');
+    setIsLoading(false)
+
+    setTitle('')
+    setHours('')
+    setMinutes('')
+    setDescription('')
+    setLabelId('')
+    setSelectedDate(null)
   }
 
   return (
     <div className={styles.modalContainer}>
       <div className={styles.container}>
-        <h3 className="text-center">Let's Add Your Task !</h3>
-        <form className={styles.formContainer} id="contactForm" name="contactForm">
+        <h3 className="text-center">Let's Add Your Task!</h3>
+        <form
+          className={styles.formContainer}
+          id="contactForm"
+          name="contactForm"
+        >
           <div className="row">
             <div className="col-md-6 form-group mb-3">
               <label className="col-form-label">Title</label>
-              <input type="text" className="form-control" name="name" id="name" placeholder="Your task title" value={title} onChange={(e) => setTitle(e.target.value)} />
+              <input
+                type="text"
+                className="form-control"
+                name="title"
+                id="title"
+                placeholder="Your task title"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+              />
             </div>
             <div className="col-md-6 form-group mb-3">
-              <label className="col-form-label">Duration</label>
-              <input type="email" className="form-control" name="email" id="email" placeholder="Estimated task duration" value={duration} onChange={(e) => setDuration(e.target.value)} />
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-md-12 form-group mb-3">
               <label className="col-form-label">Label</label>
               <select
                 className="form-control"
                 value={labelId}
-                onChange={(e) => setlabelId(e.target.value)}
+                onChange={(e) => setLabelId(e.target.value)}
               >
                 <option value="">Select Label</option>
                 {labels ? (
@@ -85,22 +99,76 @@ function AddItemForm() {
                     </option>
                   ))
                 ) : (
-                  <option disabled>Loading...</option> 
+                  <option disabled>Loading...</option>
                 )}
               </select>
             </div>
           </div>
           <div className="row">
+            <div className="col-md-6 form-group mb-3">
+              <label className="col-form-label">Date</label>
+              <div className={`form-control ${styles.datepickerContainer}`}>
+                <DatePicker
+                  className={`form-control ${styles.datepicker}`}
+                  selected={selectedDate}
+                  onChange={(date) => setSelectedDate(date)}
+                  placeholderText="Select task date"
+                  dateFormat="yyyy-MM-dd"
+                />
+              </div>
+            </div>
+            <div className="col-md-6 form-group mb-3">
+              <label className="col-form-label">Duration</label>
+              <div className="row">
+                <div className="col-md-6">
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Hours"
+                    value={hours}
+                    onChange={(e) => setHours(e.target.value)}
+                  />
+                </div>
+                <div className="col-md-6">
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Minutes"
+                    value={minutes}
+                    onChange={(e) => setMinutes(e.target.value)}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="row">
             <div className="col-md-12 form-group mb-3">
               <label className="col-form-label">Description</label>
-              <textarea className="form-control" name="message" id="message" cols="30" rows="4" placeholder="Task description" value={description} onChange={(e) => setDescription(e.target.value)}></textarea>
+              <textarea
+                className="form-control"
+                name="message"
+                id="message"
+                cols="30"
+                rows="4"
+                placeholder="Task description"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+              ></textarea>
             </div>
           </div>
         </form>
         <div className={styles.buttonContainer}>
-          <button className='btn btn-primary btn-lg' onClick={handleSubmit} disabled={isLoading}>
-            {isLoading? (
-              <span className="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+          <button
+            className="btn btn-primary btn-lg"
+            onClick={handleSubmit}
+            disabled={isLoading}
+          >
+            {isLoading ? (
+              <span
+                className="spinner-border spinner-border-sm"
+                role="status"
+                aria-hidden="true"
+              ></span>
             ) : (
               'Add Task'
             )}
@@ -108,7 +176,7 @@ function AddItemForm() {
         </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default AddItemForm;
+export default AddItemForm
--- components/SessionUser.js
+++ components/SessionUser.js
@@ -1,16 +1,16 @@
-import { getSession } from 'next-auth/react';
+import { getSession } from 'next-auth/react'
 
 export default async function SessionUser(context) {
-  const session = await getSession(context);
+  const session = await getSession(context)
 
   if (!session) {
-    return null;
+    return null
   }
 
   const user = {
     name: session.user.name,
     email: session.user.email,
-  };
+  }
 
-  return user;
+  return user
 }
