9277975f42dd4bd3b0c9742e9b19210316b1692f
Ryan <105310431+murray-dev@users.noreply.github.com>
2023-07-02 15:06:16 +0200

    Synch PieChart with category colors.

--- components/PieChart.js
+++ components/PieChart.js
@@ -1,3 +1,5 @@
+import categories from '../utils/categories'
+import { LEISURE_COLOR, WORK_COLOR, FITNESS_COLOR } from '../utils/colors'
 import dynamic from 'next/dynamic'
 // A known bug requires loading plotly dynamically in next
 const Plot = dynamic(
@@ -15,6 +17,13 @@ export default function PieChart({ data }) {
 
   const getValues = () => Object.values(data)
   const getLabels = () => Object.keys(data)
+  const getColors = (labels) =>
+    labels.map((label) => {
+      if (label === categories.FITNESS) return FITNESS_COLOR
+      else if (label === categories.WORK) return WORK_COLOR
+      else if (label === categories.LEISURE) return LEISURE_COLOR
+      else return '#0000'
+    })
 
   return (
     <Plot
@@ -25,7 +34,12 @@ export default function PieChart({ data }) {
           labels: getLabels(),
         },
       ]}
-      layout={{ title: title, width: width, height: height }}
+      layout={{
+        title: title,
+        width: width,
+        height: height,
+        colorway: getColors(getLabels()),
+      }}
       config={{ responsive: true, displaylogo: false }}
     />
   )
