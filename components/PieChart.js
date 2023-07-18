c69c4bb4ff490033c4920e37b4a368af402eb614
Ryan <105310431+murray-dev@users.noreply.github.com>
2023-07-18 14:02:46 +0200

    Create color from category util function

--- components/PieChart.js
+++ components/PieChart.js
@@ -1,5 +1,4 @@
-import categories from '../utils/categories'
-import { LEISURE_COLOR, WORK_COLOR, FITNESS_COLOR } from '../utils/colors'
+import { colorOfCategory } from '../utils/colors'
 import dynamic from 'next/dynamic'
 // A known bug requires loading plotly dynamically in next
 const Plot = dynamic(
@@ -12,12 +11,7 @@ const Plot = dynamic(
 export default function PieChart({ data, title, width }) {
   const labels = Object.keys(data).sort()
   const values = labels.map((label) => data[label])
-  const colors = labels.map((label) => {
-    if (label === categories.FITNESS) return FITNESS_COLOR
-    else if (label === categories.WORK) return WORK_COLOR
-    else if (label === categories.LEISURE) return LEISURE_COLOR
-    else return '#0000'
-  })
+  const colors = labels.map((label) => colorOfCategory(label))
 
   return (
     <Plot
--- utils/colors.js
+++ utils/colors.js
@@ -1,3 +1,12 @@
+import categories from './categories'
+
 export const LEISURE_COLOR = '#fde68a'
 export const WORK_COLOR = '#bfdbfe'
 export const FITNESS_COLOR = '#bbf7d0'
+
+export function colorOfCategory(category) {
+  if (category === categories.FITNESS) return FITNESS_COLOR
+  else if (category === categories.WORK) return WORK_COLOR
+  else if (category === categories.LEISURE) return LEISURE_COLOR
+  else return '#0000'
+}
