cb6307502444198ad271ca67f5c48c981ce4173a
Ryan <105310431+murray-dev@users.noreply.github.com>
2023-07-18 03:44:52 +0200

    Clean up empty balance page

--- pages/balance.js
+++ pages/balance.js
@@ -1,28 +1,16 @@
 import styles from '../styles/DashBoard.module.css'
-import Calendar from '../components/calendar'
 import { signOut, getSession } from 'next-auth/react'
 import { useState } from 'react'
-import ListPage from '../components/ListPage'
 import { prisma } from '../lib/db'
 import Spinner from '../components/Spinner'
 import { useRouter } from 'next/router'
 import { LABELS_PAGE, LOGIN_PAGE } from '../utils/routes'
 import { useIsClient } from '../hooks/useIsClient'
-import Modal from 'react-modal'
-import AddItemForm from '../components/AddItemForm'
-import FilterItemForm from '../components/FilterItemForm'
-import { motion } from 'framer-motion'
 import Link from 'next/link'
 import NavBarBase from '../components/NavBarBase'
 
-Modal.setAppElement('#__next')
-
 export default function Dashboard({ session, doesNotHaveLabelsSetup }) {
   const [loggingOut, setLoggingOut] = useState(false)
-  const [listURL, setListURL] = useState()
-  const [isModalOpen, setIsModalOpen] = useState(false)
-  const [isCalendarModalOpen, setIsCalendarModalOpen] = useState(false)
-  const [isFilterModalOpen, setIsFilterModalOpen] = useState(false)
 
   const router = useRouter()
   const isClient = useIsClient()
@@ -33,30 +21,6 @@ export default function Dashboard({ session, doesNotHaveLabelsSetup }) {
 
   if (isClient && !session) {
     router.push(LOGIN_PAGE)
-  }
-
-  function openModal() {
-    setIsModalOpen(true)
-  }
-
-  function closeModal() {
-    setIsModalOpen(false)
-  }
-
-  function openCalendarModal() {
-    setIsCalendarModalOpen(true)
-  }
-
-  function closeCalendarModal() {
-    setIsCalendarModalOpen(false)
-  }
-
-  function openFilterModal() {
-    setIsFilterModalOpen(true)
-  }
-
-  function closeFilterModal() {
-    setIsFilterModalOpen(false)
   }
 
   const handleLogout = async () => {
@@ -74,7 +38,7 @@ export default function Dashboard({ session, doesNotHaveLabelsSetup }) {
       <NavBarBase loggingOut={loggingOut}>
         <Link href="./dashboard">
           <button className={`${styles.button} btn btn-primary`}>
-            DashBoard
+            Dashboard
           </button>
         </Link>
         <button
