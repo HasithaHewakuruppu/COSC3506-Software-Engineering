88cd9fc7c502cceda780d963f0c6e09a8afc7389
Ryan <105310431+murray-dev@users.noreply.github.com>
2023-07-18 02:28:47 +0200

    Extract base nav bar into a component

---
+++ components/NavBarBase.js
@@ -0,0 +1,25 @@
+import styles from '../styles/DashBoard.module.css'
+import { HashLoader } from 'react-spinners'
+
+export default function NavBarBase({ children, loggingOut }) {
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles.navbarContent}>
+        <div className={`${styles.heading}`}>
+          <h1>Harmony Hub</h1>
+        </div>
+        <div className={`${styles.logoutButton}`}>
+          {loggingOut ? (
+            <HashLoader
+              size={30}
+              color="#0d6efd"
+              className={styles.logoutAnimation}
+            />
+          ) : (
+            <>{children}</>
+          )}
+        </div>
+      </div>
+    </nav>
+  )
+}
